01adf535846366355cda99517a2ab39e036408a3
Gurnoor344 <124993584+Gurnoor344@users.noreply.github.com>
2024-10-22 11:59:11 +0530

    Second Commit

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+//WHAT TO DO 
 /*
 dr  appointment ---- 
 id:, name:string
@@ -6,30 +7,35 @@ dr specialization:String
 //available slot:20  */
 
 //framework configuration 
+const express = require('express')
+const connectDb=require("./config/dbConnection")
+const errorHandler=require('./middleware/errorHandler')
+const cors=require("cors"); 
+
+//env file config [how it will work]
+const dotenv = require("dotenv");
+dotenv.config();
+
+ connectDb();
+const app=express();
+const port =process.env.port || 5000; 
 
 
-// const express = require('express')
-// const connectDb=require("./config/dbConnection")
-// const errorHandler=require('./middlewares/errorHandler')
-// const cors=require("cors"); 
+app.use(express.json());  
+app.use(cors());
+// //error handling middleware 
 
-// connectDb();
-// const app=express();
-// const port =process.env.port || 5000; 
-
-
-// app.use(express.json());
-// app.use(cors());
 
 // //routes below 
-// app.get('/',(req,res)=>{
-//     res.send("working");
-// });
+app.get('/',(req,res)=>{
+    res.send("working");
+});
 
-// //error handling middleware 
-// app.use(errorHandler)
+app.use(errorHandler)
+
 
 // //app config start 
-// app.listen(port,()=>{
-//     console.log(server running on port http://localhost:${port});
-// })
+app.listen(port,()=>{
+    console.log(`server running on port http://localhost:${port}`);
+
+})
